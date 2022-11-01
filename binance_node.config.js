c947435d7aa599b7071a6c35edf379842c3547dc
ehdckf <ehdcjf@funnyfig.com>
2022-11-01 18:39:27 +0900

    UPDATE: binance config

--- binance_node.config.js
+++ binance_node.config.js
@@ -3,8 +3,8 @@ module.exports = {
                 {
                         name: "binance_node",
                         script: "./dist/binance_node",
-                        error_file: "../../logs/binance_node_err",
-                        out_file: "../../logs/binance_node_out",
+                        error_file: "../logs/binance_node_err",
+                        out_file: "../logs/binance_node_out",
                         //			instances: 8,
                         //			exec_mode: "cluster",
                         combine_logs: "false",
